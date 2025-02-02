371e86e1e12b9cca5f484921b04142388a93c451
Vanshita Khantal <khantalvanshita@gmail.com>
2025-02-02 18:06:03 +0530

    Constructor, Variables, Access Modifiers Problem 9 Committed

--- Book1.cs
+++ Book1.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+
+class Book
+{
+    // Properties
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public double Price { get; set; }
+
+    // Default Constructor
+    public Book()
+    {
+        Title = "Introduction to Csharp";
+        Author = "Eric Gunnerson";
+        Price = 800;
+    }
+
+    // Parameterized Constructor
+    public Book(string title, string author, double price)
+    {
+        this.Title = title;
+        this.Author = author;
+        this.Price = price;
+    }
+
+    // Method to Display Book Details
+    public void Display()
+    {
+        Console.WriteLine($"Title: {Title}, Author: {Author}, Price: ${Price}");
+    }
+}
