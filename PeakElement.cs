adac5243d5fb6d0d5bb623faeb2e45e6efd582f9
Vanshita Khantal <khantalvanshita@gmail.com>
2025-02-16 15:53:03 +0530

    [Vanshita Khantal]Add. Searching problems committed

---
+++ PeakElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 3, 5, 4, 2 };
+        int peakIndex = FindPeakElement(array);
+        
+        Console.WriteLine($"A peak element is at index: {peakIndex}, value: {array[peakIndex]}");
+    }
+    
+    static int FindPeakElement(int[] arr)
+    {
+        int left = 0, right = arr.Length - 1;
+        
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            
+            if (arr[mid] > arr[mid + 1])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
