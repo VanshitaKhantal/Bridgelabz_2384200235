b18456cf7fa59ea434414307eab96e662cbbe7dd
Vanshita Khantal <khantalvanshita@gmail.com>
2025-03-22 21:55:27 +0530

    [Vanshita] Add: Angular Bindings

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Helloworld';
+  imgUrl = "assets/BridgeLabz-Logo.png";
+  url = "https://www.bridgelabz.com";
+  userName:string = "";
+  nameError:string = "";
+ 
+  ngOnInit(): void {
+  
+  this.title = "Hello from BridgeLabz.";
+  
+  }
+
+  onClick($event) {
+
+    console.log("Save button is clicked!", $event);
+    
+    window.open(this.url, "_blank");
+    
+    }
+
+    onInput($event){
+
+      console.log("Change Event Occurred!", $event.data);
+      
+      const nameRegex = RegExp('^ [A-Z]{1} [a-zA-Z\\s]{2,}5');
+      
+      if (nameRegex.test(this.userName)) {
+      
+      this.nameError = "";
+      
+      return;
+      
+      }
+      
+      this.nameError = "Name is Incorrect!";
+    }
+  
+}
+  
